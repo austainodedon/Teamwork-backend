2a26e16d028ef762d33f159c741043c1433d5c8b
Austaino <c3ono32@yahoo.com>
2019-11-11 01:05:35 -0800

    feature(employee-can-post-gifs):employee can post gifs employee can post gifs the employee should be signed in [starts]

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
+const bodyParser = require("body-parser");
 const express = require("express");
-
+const users = require("./routes/user.route");
+const gifs = require("./routes/gif.route");
 const app = express();
+
+app.use(bodyParser.json());
+app.use("/v1/auth", users);
+app.use("/v1/gifs", gifs);
 
 app.get("/", (req, res) => {
   res.send("Hello this is my Teamwork App");
