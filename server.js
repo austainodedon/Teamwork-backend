1efd1db25c2be06ea0d19e1ae0a8792a06c0f821
Austaino <c3ono32@yahoo.com>
2019-11-12 01:25:59 -0800

    feature(employee-view-article-category):employee can view all articles in a category employee can view all articles in a given category [starts]

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const users = require("./routes/user.route");
 const gifs = require("./routes/gif.route");
+const categories = require("./routes/category.route");
+const articles = require("./routes/article.route");
 const app = express();
 
 app.use(bodyParser.json());
 app.use("/v1/auth", users);
 app.use("/v1/gifs", gifs);
+app.use("/v1/articles", articles);
+app.use("/v1/categories", categories);
 
 app.get("/", (req, res) => {
   res.send("Hello this is my Teamwork App");
